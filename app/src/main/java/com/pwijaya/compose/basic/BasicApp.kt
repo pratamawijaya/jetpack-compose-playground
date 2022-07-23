42d44f113c6c97d262ccdb152fbed7511d990e38
pratamawijaya <set.mnemonix@gmail.com>
2022-07-23 12:08:13 +0700

    add lazycolumn

--- app/src/main/java/com/pwijaya/compose/basic/BasicApp.kt
+++ app/src/main/java/com/pwijaya/compose/basic/BasicApp.kt
@@ -3,6 +3,8 @@ package com.pwijaya.compose.basic
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Surface
@@ -21,16 +23,26 @@ fun MyBasicApp(names: List<String> = listOf("World", "Compose")) {
         mutableStateOf(true)
     }
 
-    if(shouldShowOnboarding){
-        OnboardingScreen (onContinueClicked = {shouldShowOnboarding = false})
-    }else{
-        Column(modifier = Modifier.padding(vertical = 4.dp)) {
-            for (name in names) {
-                Greeting(name = name)
-            }
-        }
+    if (shouldShowOnboarding) {
+        OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
+    } else {
+        Greetings()
+//        Column(modifier = Modifier.padding(vertical = 4.dp)) {
+//            for (name in names) {
+//                Greeting(name = name)
+//            }
+//        }
     }
 
+}
+
+@Composable
+private fun Greetings(names: List<String> = List(1000) { "$it" }) {
+    LazyColumn(modifier = Modifier.padding(vertical = 4.dp)) {
+        items(items = names) { name ->
+            Greeting(name = name)
+        }
+    }
 }
 
 @Composable
@@ -38,23 +50,25 @@ private fun Greeting(name: String) {
     val expanded = remember {
         mutableStateOf(false)
     }
-    val extraPadding = if(expanded.value) 48.dp else 0.dp
+    val extraPadding = if (expanded.value) 48.dp else 0.dp
 
     Surface(
         color = MaterialTheme.colors.primary,
         modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
         Row(modifier = Modifier.padding(24.dp)) {
-            Column(modifier = Modifier
-                .weight(1f)
-                .padding(bottom = extraPadding)) {
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(bottom = extraPadding)
+            ) {
                 Text(text = "Hello,")
                 Text(text = name)
             }
             OutlinedButton(onClick = {
                 expanded.value = !expanded.value
             }) {
-                Text(if(expanded.value) "Show less" else "Show More")
+                Text(if (expanded.value) "Show less" else "Show More")
             }
         }
 
