09ea3d3e4b4ac8f087b55dc95cf1331c46ee10d4
pratamawijaya <set.mnemonix@gmail.com>
2022-07-23 11:34:25 +0700

    add sample basic compose

---
+++ app/src/main/java/com/pwijaya/compose/basic/BasicApp.kt
@@ -0,0 +1,52 @@
+package com.pwijaya.compose.basic
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedButton
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.pwijaya.compose.ui.theme.JetpackComposePlaygroundTheme
+
+@Composable
+fun MyBasicApp(names: List<String> = listOf("World", "Compose")) {
+    Column(modifier = Modifier.padding(vertical = 4.dp)) {
+        for (name in names) {
+            Greeting(name = name)
+        }
+    }
+}
+
+@Composable
+private fun Greeting(name: String) {
+    Surface(
+        color = MaterialTheme.colors.primary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)) {
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(modifier = Modifier.weight(1f)) {
+                Text(text = "Hello,")
+                Text(text = name)
+            }
+            OutlinedButton(onClick = {
+
+            }) {
+                Text("Show More")
+            }
+        }
+
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320)
+@Composable
+fun DefaultPreview() {
+    JetpackComposePlaygroundTheme{
+        MyBasicApp()
+    }
+}
